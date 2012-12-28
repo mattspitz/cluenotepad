fe253ba798be6e8ecf67fa010652d9e9f77d96f0
Matt Spitz <mspitz@ebay.com>
2012-12-28 11:28:43 -0800

    Refactoring turn handling to allow specific card shown to be input as well.

--- cluenotepad.py
+++ cluenotepad.py
@@ -12,26 +12,26 @@ import Levenshtein
 
 _logger = logging.getLogger(__name__)
 
-class Board:
-    people = [
+BOARD = {
+    "people": [
             "Mustard",
             "Plum",
             "Green",
             "Peacock",
             "Scarlet",
             "White"
-            ]
+            ],
 
-    weapons = [
+    "weapons": [
             "Knife",
             "Candlestick",
             "Revolver",
             "Rope",
-            "LeadPipe",
+            "Pipe",
             "Wrench"
-            ]
+            ],
 
-    rooms = [
+    "rooms": [
             "Hall",
             "Lounge",
             "DiningRoom",
@@ -42,9 +42,10 @@ class Board:
             "Library",
             "Study"
             ]
+    }
 
 Question = namedtuple("Question", "person weapon room")
-Turn = namedtuple("Turn", "question asker answerer")
+Turn = namedtuple("Turn", "question asker answerer card")
 
 class Game(object):
     def __init__(self, players, known_cards):
@@ -59,9 +60,19 @@ class Game(object):
         self.turns.appendleft(turn)
 
     def print_current_state(self):
-        """ Prints the current game state, rebuilt from scratch from all turns. """
-        # TODO construct game state
-        print self.known_cards
+        """ Prints the current game state, rebuilt from scratch from all turns.
+
+        For each item-player combination, we store one of three states, YES, NO, or MAYBE.  A next iteration could be smart about which questions to ask next to reveal more information, but we're not there yet.
+        """
+        YES, NO, MAYBE = range(1, 4)
+
+        game_state = defaultdict(lambda: defaultdict(dict))
+        for group, items in BOARD.iteritems():
+            for item in items:
+                for player in self.players:
+                    game_state[group][item][player] = MAYBE
+
+        self.known_cards
         print("Players: {0}".format(", ".join(self.players)))
         # TODO print game state
 
@@ -79,27 +90,45 @@ def find_best(s, options):
     """ Given a raw input string and a series of possibilities, returns the option with the lowest Levenshtein distance, representing our best guess. """
     return sorted(options, key=lambda opt: Levenshtein.distance(s.lower(), opt.lower()))[0]
 
+class EndOfGameException(Exception):
+    pass
+
 def get_turn(players):
     while True:
         try:
-            raw_turn = raw_input("Enter turn as 'asker person weapon room answerer': ")
-            asker, person, weapon, room, answerer = map(lambda s: s.strip(), raw_turn.split())
-            break
+            raw_turn = raw_input("Enter turn as 'asker person weapon room answerer [card]': ")
+            split = map(lambda s: s.strip(), raw_turn.split())
+            if len(split) == 5:
+                asker, person, weapon, room, answerer = split
+                card = None
+            elif len(split) == 6:
+                asker, person, weapon, room, answerer, card = split
+            else:
+                raise Exception("Not enough information specified.")
+
+            question = Question(find_best(person, BOARD["people"]),
+                    find_best(weapon, BOARD["weapons"]),
+                    find_best(room, BOARD["rooms"]))
+            best_card = find_best(card, itertools.chain(*BOARD.itervalues())) if card is not None else None
+            if best_card is not None and best_card not in (question.person, question.weapon, question.room):
+                raise Exception("Card shown ({}) must be one from the question ({})".format(best_card, question))
+
+            return Turn(question,
+                    find_best(asker, players),
+                    find_best(answerer, players + ["-"]), # - indicates that no one answered it
+                    best_card)
+        except EOFError:
+            raise EndOfGameException()
         except Exception:
             logging.exception("Couldn't parse input string")
-
-    return Turn(
-            Question(find_best(person, Board.people),
-                find_best(weapon, Board.weapons),
-                find_best(room, Board.rooms)),
-            find_best(asker, players),
-            find_best(answerer, players + ["-"]) # - indicates that no one answered it
-            )
 
 def game_loop(game=None):
     while True:
         game.print_current_state()
-        turn = get_turn(game.players)
+        try:
+            turn = get_turn(game.players)
+        except EndOfGameException:
+            return
         game.add_turn(turn)
         game.dump_state()
 
